4cf54244505870088372807149e9294c255aa938
filipedevprogramming384 <filipe.dev.programming@gmail.com>
2023-11-16 02:10:48 -0300

    Desafio1

---
+++ desafio_sentenca_bloco.js
@@ -0,0 +1,29 @@
+
+//comando para comentar várias linhas.
+    //seleciona todas as linhas e pressiona crtl+/
+    
+
+    
+/* Mostrar na tela números impares de 0 a 9 e números pares de 0 a 9*/
+
+//números impares
+{
+    console.log('números impares');
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+//números pares
+{
+    console.log('números pares');
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+
+}
+
