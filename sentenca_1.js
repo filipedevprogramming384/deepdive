43980e61b9ce6341511e923f51f86a2d8143ecb7
filipedevprogramming384 <filipe.dev.programming@gmail.com>
2023-11-16 00:32:43 -0300

    Estudo sobre blocos

---
+++ sentenca_1.js
@@ -0,0 +1,10 @@
+//prestar muita atenção na sequência do código.  Ele vai executar linha por linha
+
+//imprime um valor
+console.log(1);
+
+//imprime o resultado de uma operação
+console.log(1+1);
+
+//podemos usar aspas simples ou aspas dupla.
+console.log('texto');
