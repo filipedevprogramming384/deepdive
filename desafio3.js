09b918bd149d4bacd3284c6c89d40d8ca0dfdde1
filipedevprogramming384 <filipe.dev.programming@gmail.com>
2023-11-16 02:52:02 -0300

    desafio 3

---
+++ desafio3.js
@@ -0,0 +1,14 @@
+//troca de valores
+
+let a = 7;
+let b = 94
+
+console.log('valor antes da troca a = '+ a + '  b = '+b);
+console.log('depois da troca');
+
+let c = a;
+a = b;
+b = c;
+
+console.log('a = '+ a + '  b = '+b);
+
