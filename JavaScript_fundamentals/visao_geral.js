8975b109e2d8006f7c3e9912cf949543f35e7dbf
filipe.dev.programming384 <filipe.dev.programming@gmail.com>
2023-11-16 17:54:14 -0300

    Aula algoritmos

---
+++ JavaScript_fundamentals/visao_geral.js
@@ -0,0 +1,8 @@
+/* Conjuntos de passos encadeados de forma lógica para resolver um determinado problema*/
+/*possui estruturas de repetição, tomada de decisão, .....
+
+// a sequência dever ser lógica e coerente..
+
+// Sequência básica 
+
+        //Entrada -->  Processamento --> Saída
