17e3d9552054623a6db2b5ba4a268ccd3299576e
filipedevprogramming384 <filipe.dev.programming@gmail.com>
2023-11-16 02:26:30 -0300

    Aula sobre dados

---
+++ dados.js
@@ -0,0 +1,22 @@
+//Variaveis
+
+    var quantidade = 10;
+    var preco_unitario = 6.5;
+
+    //let também cria variáveis
+    let preco_final;
+    
+
+    console.log(quantidade);
+    console.log(preco_unitario);
+
+    preco_final = quantidade * preco_unitario;
+
+    console.log('valor total', preco_final);
+
+
+    //identificador
+        //escolher bom nomes é importante|
+
+    
+    
